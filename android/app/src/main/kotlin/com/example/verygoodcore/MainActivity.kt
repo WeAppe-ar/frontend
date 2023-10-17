e7a555f723e9ad03b103024286ad556d4dadb98e
Tomás Sasovsky <to.sasovsky@gmail.com>
2023-10-17 19:02:15 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.weappear_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:weappear_frontend/app/app.dart';
+import 'package:weappear_frontend/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
